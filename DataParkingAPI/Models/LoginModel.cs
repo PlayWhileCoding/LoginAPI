06290fe3a4093c6175b7bfbb484c978f2df4e116
“aldo.castillo” <aldo.castillo@accenture.com>
2021-09-13 11:48:40 -0500

    Modified base code to allow CORS

--- DataParkingAPI/Models/LoginModel.cs
+++ DataParkingAPI/Models/LoginModel.cs
@@ -6,17 +6,17 @@ using System.Web;
 
 namespace DataParkingAPI.Models
 {
-    public class LoginModel
+    public class UserModel
     {
        
             [Required(ErrorMessage = "Username is required")]
             [StringLength(30, ErrorMessage = "Maximum Lenght: 15", MinimumLength = 8)]
-            public string UserName { get; set; }
+            public string userName { get; set; }
 
             [Required(ErrorMessage = "Password is required")]
             [DataType(DataType.Password)]
             [StringLength(30, ErrorMessage = "Maximum Lenght: 12 ", MinimumLength = 8)]
-            public string Password { get; set; }
+            public string password { get; set; }
         }
    
 }
